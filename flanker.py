a3ab7b408afffb261a217e3f98ce5109a0d79f58
clarissamont <78494077+clarissamont@users.noreply.github.com>
2021-05-12 10:11:13 +0200

    Update flanker.py

--- flanker.py
+++ flanker.py
@@ -3,14 +3,17 @@
 
 This code runs the "Flanker Task" experiment, where the participant is 
 presented with one of 3 types of stimuli: 
-    
+  
+  
 CONGRUENT: >>>>> or <<<<<
 INCONGRUENT: <<><< or >><>>
 NEUTRAL: ||<|| or ||>||
 
+
 The participant clicks the arrow keys (RIGHT or LEFT) corresponding to
 the arrow presented in the MIDDLE of the stimulus, regardless of the 
 surrounding symbols/arrows.
+
 
 """
 
